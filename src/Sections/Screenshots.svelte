8dcce19d62f56cd5e5d6196ef5ee9fab8eed0d4e
Silas Reinagel <silas.reinagel@carvana.com>
2020-07-08 17:20:14 -0700

    Screenshots are lazy loaded

--- src/Sections/Screenshots.svelte
+++ src/Sections/Screenshots.svelte
@@ -1,25 +1,30 @@
 <script>
   import TwoColumnLayout from '../Elements/TwoColumnMobileLayout.svelte';
+  import Lazy from '../Elements/Lazy.svelte';
   import project from '../static-content';
 
   const screenshots = project.screenshots;
 
   const leftImgs = screenshots.filter((_,i) => i % 2 === 0);
   const rightImgs = screenshots.filter((_,i) => i&1);
+
+  const skeletonHeight = leftImgs.length * 300;
 </script>
 
-<TwoColumnLayout>
-  <div slot="first">
-    {#each leftImgs as image}
-      <a href={image}><div class='screenshot'><img  src={image} alt='Screenshot' lazy='true'/></div></a>
-    {/each}
-  </div>
-  <div slot="second">
-    {#each rightImgs as image}
-      <a href={image}><div class='screenshot'><img  src={image} alt='Screenshot' lazy='true'/></div></a>
-    {/each}
-  </div>
-</TwoColumnLayout>
+<Lazy height={skeletonHeight}>
+  <TwoColumnLayout>
+    <div slot="first">
+      {#each leftImgs as image}
+        <a href={image}><div class='screenshot'><img  src={image} alt='Screenshot' lazy='true'/></div></a>
+      {/each}
+    </div>
+    <div slot="second">
+      {#each rightImgs as image}
+        <a href={image}><div class='screenshot'><img  src={image} alt='Screenshot' lazy='true'/></div></a>
+      {/each}
+    </div>
+  </TwoColumnLayout>
+</Lazy>
 
 <style>
   .screenshot {
